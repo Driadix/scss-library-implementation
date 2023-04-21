36bbc8fd6e9a986de62e4f3c5b1d6a8c61b4d563
Driadix <59824288+Driadix@users.noreply.github.com>
2023-04-21 16:39:53 +0300

    feature: create navbar component, set vars on def, base html markup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
+const purgecss = require('gulp-purgecss');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('my-library/**/*.scss').pipe(sass()).pipe(dest('css'));
+  return src('my-library/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'));
 }
 
 function watchStyles() {
-  watch(['my-library/**/*.scss'], buildStyles);
+  watch(['my-library/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchStyles);
